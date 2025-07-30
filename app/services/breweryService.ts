708b4c029813e073ebbc22e22c3ac910a81f3f51
raul.bana <raulfbana@gmail.com>
2025-07-30 18:52:55 -0300

    feat: implement brewery service and API routes

---
+++ app/services/breweryService.ts
@@ -0,0 +1,39 @@
+import axios from "axios"
+import { Brewery } from "../types";
+
+const apiInstance = axios.create({
+    baseURL: 'https://api.openbrewerydb.org/v1',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+
+export const breweriesService = {
+    getAllBreweries: async (): Promise<Brewery[]> => {
+        try {
+            const response = await apiInstance.get(apiRoutes.brewery.getAll);
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching breweries:', error);
+            throw error;
+        }
+    },
+    getBreweryById: async (id: string): Promise<Brewery | null> => {
+        try {
+            const response = await apiInstance.get(apiRoutes.brewery.getById(id));
+            return response.data;
+        } catch (error) {
+            console.error(`Error fetching brewery with ID ${id}:`, error);
+            throw error;
+        }
+    },
+    searchBreweries: async (query: string): Promise<Brewery[]> => {
+        try {
+            const response = await apiInstance.get(apiRoutes.brewery.search(query));
+            return response.data;
+        } catch (error) {
+            console.error('Error searching breweries:', error);
+            throw error;
+        }
+    }
+}
